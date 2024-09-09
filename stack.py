0d79bfd6cf975229171040b45dd40c5cc1717de3
nfgcode <nurfauzangymnastiar@gmail.com>
2024-09-09 23:08:52 +0800

    Fungsi pop, Bisa menghilangkan salah satu output swap

--- stack.py
+++ stack.py
@@ -15,13 +15,40 @@ class Stack:
         new_node.next = self.first
         self.first = new_node
 
-    def pop(self):
-        if self.hasPop():
-            popped_value = self.first.value
-            self.first = self.first.next
-            return popped_value
+    def pop(self, index=None):
+        if index is None:
+            if self.hasPop():
+                popped_value = self.first.value
+                self.first = self.first.next
+                return popped_value
+            else:
+                return None
         else:
-            return None
+            if index < 1:
+                print("Invalid index")
+                return
+            stack = self.first
+            count = 1
+            node = None
+            while stack:
+                if count == index:
+                    node = stack
+                    break
+                count += 1
+                stack = stack.next
+            if node:
+                popped_value = node.value
+                if index == 1:
+                    self.first = self.first.next
+                else:
+                    prev_node = self.first
+                    for _ in range(index - 2):
+                        prev_node = prev_node.next
+                    prev_node.next = node.next
+                return popped_value
+            else:
+                print("Invalid index")
+                return
 
     def swap(self, position1, position2):
         if position1 < 1 or position2 < 1:
@@ -76,7 +103,7 @@ my_stack.swap(3, 5)
 print("Stack after swapping:")
 my_stack.print_stack()
 
-print("Popped element:", my_stack.pop())
+print("Popped element:", my_stack.pop(3))
 
 print("Stack after popping:")
 my_stack.print_stack()
